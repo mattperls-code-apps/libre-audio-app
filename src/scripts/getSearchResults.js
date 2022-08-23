e82779b57a01b7ab6917844ebff501c7965e59e9
Rafayel <67717700+Gitter499@users.noreply.github.com>
2022-08-23 17:28:29 +0000

    ~ Refractored getSearchResults script ~ Added unstylized loading bar

--- src/scripts/getSearchResults.js
+++ src/scripts/getSearchResults.js
@@ -4,42 +4,56 @@ import formatArtist from "./formatArtist"
 import { timeStringToLength } from "./timeUtils"
 
 const getSearchResults = (query, callback) => {
-    const url = `https://www.youtube.com/results?q=${encodeURI(query)}&hl=en`
-        
-    axios.get(url).then(res => {
-        const data = JSON.parse(res.data.split("ytInitialData = '")[1].split("';</script>")[0].replace(/\\x([0-9A-F]{2})/ig, (...items) => {
-            return String.fromCharCode(parseInt(items[1], 16));
-        }).replaceAll("\\\\\"", ""))
+  let _videos = []
+  
+  const url = `https://www.youtube.com/results?q=${encodeURI(query)}&hl=en`
 
-        let searchResults = data?.contents?.sectionListRenderer?.contents?.[0]?.itemSectionRenderer?.contents
+  axios.get(url).then(res => {
+    const data = JSON.parse(res.data.split("ytInitialData = '")[1].split("';</script>")[0].replace(/\\x([0-9A-F]{2})/ig, (...items) => {
+      return String.fromCharCode(parseInt(items[1], 16));
+    }).replaceAll("\\\\\"", ""))
 
-        if(searchResults == undefined){
-            searchResults = data?.contents?.sectionListRenderer?.contents?.[1]?.itemSectionRenderer?.contents
-        }
-        
-        const rawVideoResults = searchResults.filter(item => item.hasOwnProperty("compactVideoRenderer")).map(item => item.compactVideoRenderer)
-        
-        const videos = []
-        rawVideoResults.forEach(video => {
-            try {
-                const labelParts = video.accessibility.accessibilityData.label.split("Go to channel - ")
-                const artist = formatArtist(labelParts[labelParts.length - 1].split(" - ")[0])
-                videos.push({
-                    id: video.videoId,
-                    title: JSON.parse(`"` + video.title.runs[0].text + `"`),
-                    artist,
-                    thumbnail: video.thumbnail.thumbnails[video.thumbnail.thumbnails.length - 1].url,
-                    duration: timeStringToLength(video.lengthText.runs[0].text)
-                })
-            } catch(err){
-                console.warn(err)
-            }
+    let searchResults = data?.contents?.sectionListRenderer?.contents?.[0]?.itemSectionRenderer?.contents
+
+    if (searchResults == undefined) {
+      searchResults = data?.contents?.sectionListRenderer?.contents?.[1]?.itemSectionRenderer?.contents
+    }
+
+    const rawVideoResults = searchResults.filter(item => item.hasOwnProperty("compactVideoRenderer")).map(item => item.compactVideoRenderer)
+
+    const videos = []
+    rawVideoResults.forEach(video => {
+      try {
+        const labelParts = video.accessibility.accessibilityData.label.split("Go to channel - ")
+        const artist = formatArtist(labelParts[labelParts.length - 1].split(" - ")[0])
+        videos.push({
+          id: video.videoId,
+          title: JSON.parse(`"` + video.title.runs[0].text + `"`),
+          artist,
+          thumbnail: video.thumbnail.thumbnails[video.thumbnail.thumbnails.length - 1].url,
+          duration: timeStringToLength(video.lengthText.runs[0].text)
         })
-        callback(videos)
-    }).catch((err) => {
+      } catch (err) {
         console.warn(err)
-        callback([])
+      }
     })
+    
+    if (!callback) {
+      _videos = videos
+    } else {
+      callback(videos)
+    }
+    
+  }).catch((err) => {
+    console.warn(err)
+    if (!callback) {
+      _videos = []
+    } else {
+      callback([])
+    }
+  })
+
+  if (!callback) return _videos
 }
 
 export default getSearchResults
